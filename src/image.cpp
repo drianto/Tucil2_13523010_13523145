15b12b703e1dabf69b95f18aabedebc1e9af410d
qodriazka <19623172@std.stei.itb.ac.id>
2025-04-11 00:11:05 +0700

    feat: add RGBtoImage

--- src/image.cpp
+++ src/image.cpp
@@ -6,8 +6,10 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.hpp"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.hpp"
 
-void imageToRGB() {
+void imageToRGB(std::string& filePath) {
     int width, height, channels;
 
     unsigned char* image = stbi_load(filePath.c_str(), &width, &height, &channels, 3);
@@ -31,4 +33,22 @@ void imageToRGB() {
     }    
 
     stbi_image_free(image);
+}
+
+void RGBToImage(std::vector<Pixel>& blok, std::string& outputPath, int height, int width) {
+    std::vector<unsigned char> imageData(width * height * 3);
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int idx = (y * width + x) * 3;
+            const Pixel& pixel = blok[y * width + x];
+            imageData[idx] = static_cast<unsigned char>(pixel.r);
+            imageData[idx + 1] = static_cast<unsigned char>(pixel.g);
+            imageData[idx + 2] = static_cast<unsigned char>(pixel.b);
+        }
+    }
+    if (stbi_write_jpg(outputPath.c_str(), width, height, 3, imageData.data(), 100)) {
+        std::cout << "Gambar berhasil disimpan ke: " << outputPath << std::endl;
+    } else {
+        std::cerr << "Gagal menyimpan gambar." << std::endl;
+    }
 }
